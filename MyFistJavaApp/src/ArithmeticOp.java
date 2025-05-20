5c1a8fdfc86d192df72bc8e94d6507e073f18a77
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-05-20 09:12:29 +0800

    Add files via upload

---
+++ MyFistJavaApp/src/ArithmeticOp.java
@@ -0,0 +1,57 @@
+
+public class ArithmeticOp {
+
+	public static void main(String[] args) {
+		
+		// ORDER OF OPERATIONS [P-E-M-D-A-S]
+		
+		double result = 7;
+		
+		System.out.println(result);
+		
+		
+		
+		// Increment and Decrement Operators
+		
+		//int x = 1;
+		
+		//x += 1; 
+		
+//		x++;
+//		x++;
+//		x++;
+//		x--;
+//		
+//		
+//		System.out.println(x);
+		
+		// Augmented Assignment Operators
+		
+		//int x = 10;
+		//int y = 3;
+		
+		//x += y;
+		//x -= y;
+		//x *= y;
+		//x /=  y;
+		//x %= y;
+		
+		//System.out.println(x);
+		
+		
+		
+		// Arithmetic Operators
+		
+		//int z;
+		
+		//z = x + y;
+		//z = x - y;
+		//z = x * y;
+		//z = x / y;
+		//z = x % y;
+		
+		//System.out.println(z);
+
+	}
+
+}
